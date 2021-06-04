d5b667e42ef211416b5716f8ad2d224e574fbd22
Refan Bekti <Refan Bekti@DESKTOP-T4JQ525>
2021-06-04 11:12:55 +0700

    Add project files.

---
+++ miligolas web/Scripts/General.js
@@ -0,0 +1,23 @@
+﻿function setFont() {
+    $.getScript('../Account/jquery.min.js', function () {
+        // script is now loaded and executed.
+        // put your dependent JS here.
+        //var font = 'Calibri';
+        var font = 'Algerian';
+        $("body").css('font-family', font);
+        $("span").css('font-family', font);
+        $("input").css('font-family', font);
+        $("button").css('font-family', font);
+        $("table").css('font-family', font);
+        $("label").css('font-family', font);
+        $("th").css('font-family', font);
+        $("div").css('font-family', font);
+        $(".label").css('font-family', font);
+        $("h3").css('font-family', font);
+        $("html").css('font-family', font);
+        $("iframe").css('font-family', font);
+        $("iframe").css('font-family', font);
+    });
+   
+};
+
